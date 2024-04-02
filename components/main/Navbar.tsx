ad9d29a71e1d87f3e0238a5b1df5e8691ec90274
nisarganag <nisarga.nag.cse24@heritageit.edu.in>
2024-04-02 12:56:51 +0530

    changes

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
           className="h-auto w-auto flex flex-row items-center"
         >
           <Image
-            src="/WebChainLogo.png"
+            src="/WebchainLogo.png"
             alt="logo"
             width={70}
             height={70}
